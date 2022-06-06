5cb512c3f759c1a86c48d5658b5dce8ecc6ad90d
vpopova <vpopova@provectus.som>
2022-06-06 11:50:52 +0300

    task 2

---
+++ src/com/provectus/cfk/circleArea.java
@@ -0,0 +1,12 @@
+package com.provectus.cfk;
+
+public class circleArea {
+    public static void main(String[] args) {
+        int a = 5, b = 2;
+        //double a1 = 2.5, b1 = 3;
+        double pi = 3.14;
+
+        System.out.println(pi * Math.pow(a,b));
+        //System.out.println((double) Math.pow(a1,b1));
+    }
+}
---
+++ src/com/provectus/cfk/triangle.java
@@ -0,0 +1,9 @@
+package com.provectus.cfk;
+
+public class triangle {
+    public static void main(String[] args) {
+        int a = 7, b = 6;
+        double S = 0.5 * (a * b);
+        System.out.println(S);
+    }
+}
