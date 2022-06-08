7a157826232408423fedd97cb0c4408e20fe5b9e
vpopova <vpopova@provectus.som>
2022-06-08 22:45:10 +0300

    task 2 Conditions

--- src/task3/CheckOddEven.java
+++ src/task3/CheckOddEven.java
@@ -3,14 +3,26 @@ package task3;
 public class CheckOddEven {
     public static void main(String[] args) {
 
-        int number1 = 13;
-        int number2 = 8;
+        // condition  if..else if
 
-        if (number1 % 2 == 0) {
-            System.out.println("BYE Odd Number");
-        } else if (number2 % 2 == 0) {
-            System.out.println(" BYE Even Number");
-        }
+//        int number1 = 13;
+//        int number2 = 8;
+//
+//        if (number1 % 2 == 0) {
+//            System.out.println("BYE Odd Number");
+//        } else if (number2 % 2 == 0) {
+//            System.out.println(" BYE Even Number");
+//        }
+
+
+        // ternary
+
+        int number = 5;
+
+        String result;
+        result = number % 2 == 0 ? "BYE Even Number" : "BYE Odd Number" ;
+        System.out.println(result);
+
 
 
 
