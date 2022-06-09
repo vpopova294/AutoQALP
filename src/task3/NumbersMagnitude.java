65ecdf1a06b4c4eafaf9ff98b551f4ef3e55fff5
vpopova <vpopova@provectus.som>
2022-06-09 16:37:31 +0300

    task 2 Conditions

--- src/task3/NumbersMagnitude.java
+++ src/task3/NumbersMagnitude.java
@@ -1,16 +1,32 @@
 package task3;
 
-public class NumbersMagnitude {
+
+
+    public class NumbersMagnitude {
     public static void main(String[] args) {
-        int number1 = -10;
-        int number2 = 4;
+        int number1 = 4;
+        int number2 = -10;
 
-
-        if (number1 < number2) {
-            System.out.println(Math.abs(number2) + " has less magnitude");
-        } else if (number1 > number2) {
-            System.out.println(Math.abs(number2) + " has more magnitude");
+        if (Math.abs(number1) > Math.abs(number2)) {
+            System.out.println(number2 + " has less magnitude");
+        } else if (Math.abs(number1) < Math.abs(number2)) {
+            System.out.println(number2 + " has more magnitude");
         } else
             System.out.println("the same ones");
     }
 }
+
+//  public class NumbersMagnitude {
+//    public static void main(String[] args) {
+//        int number1 = -10;
+//        int number2 = 4;
+//
+//
+//        if (number1 < number2) {
+//            System.out.println(Math.abs(number2) + " has less magnitude");
+//        } else if (number1 > number2) {
+//            System.out.println(Math.abs(number2) + " has more magnitude");
+//        } else
+//            System.out.println("the same ones");
+//    }
+//}
