4f83f2259d2098d665ef790af4e3ee5613512e1b
vpopova <vpopova@provectus.som>
2022-06-13 11:09:54 +0300

    task 2 Conditions

---
+++ src/task4/SinX.java
@@ -0,0 +1,12 @@
+package task4;
+
+public class SinX {
+    public static void main(String[] args) {
+
+
+        for (int x = 0; x < 370; x+=10) {
+
+            System.out.println("\nЗначение sin " + x + ": " + Math.sin(x));
+        }
+    }
+}
